3cef55b1f10bb61effee0960495967c59ad3dd51
Daniel Palmdal <daniel.palmdal@gmail.com>
2023-03-04 18:02:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const v = "hsah"
 const hehe = "wadup"
 const nfdksa = "fndksa"
 const haha = "jkfndsk"
+const bullshiiiit = "fksjnafa"
+const bulkndfa = "nfksaf"
+
+function hello() {
+    const hello = "©"
+}
